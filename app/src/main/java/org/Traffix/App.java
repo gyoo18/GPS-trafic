41d5a35c891037e0b51d814f461dc0efe49a8f8b
gyoo <vincent_godbout@cgocable.ca>
2025-04-05 17:51:25 -0400

    Renommé le projet à Traffix

---
+++ app/src/main/java/org/Traffix/App.java
@@ -0,0 +1,13 @@
+package org.Traffix;
+
+import javax.swing.JFrame;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        JFrame jframe = new JFrame();
+        jframe.setVisible(true);
+        System.out.println("Goodbye World!");
+    }
+}
