ec7549ea0a3e85015970caed96624ad5bb33f655
gyoo <vincent_godbout@cgocable.ca>
2025-04-24 17:36:59 -0400

    Ajouté Réseau

---
+++ app/src/main/java/org/Traffix/circulation/Réseau.java
@@ -0,0 +1,8 @@
+package org.Traffix.circulation;
+
+import java.util.ArrayList;
+
+public class Réseau {
+    public ArrayList<Route> routes = new ArrayList<>();
+    public ArrayList<Intersection> intersections = new ArrayList<>();
+}
