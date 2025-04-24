a760ba581cd7a8ff08c83e8f7d594726f8470be1
gyoo <vincent_godbout@cgocable.ca>
2025-04-24 18:19:27 -0400

    WIP

---
+++ app/src/main/java/org/Traffix/circulation/UsineRéseau.java
@@ -0,0 +1,18 @@
+package org.Traffix.circulation;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class UsineRéseau {
+    
+
+    public static Réseau générerRéseau(){
+        ArrayList<Intersection> intersectionsActives = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            
+        }
+        return null;
+    }
+
+}
