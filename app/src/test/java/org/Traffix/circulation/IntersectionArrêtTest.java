434ab6615dd385b4b1f5d338447693bb3998f727
serigne <noreply@mail.com>
2025-04-27 13:46:38 -0400

    revoir

--- app/src/test/java/org/Traffix/circulation/IntersectionArrêtTest.java
+++ app/src/test/java/org/Traffix/circulation/IntersectionArrêtTest.java
@@ -456,12 +456,6 @@ public class IntersectionArrêtTest {
         }
     }
 
-   @Test
-    public void testAddition() {
-        int result = 2 + 3;
-        assertEquals(5, result);
-    }
-
     
 public class IntersectionArrêetTest {
 
